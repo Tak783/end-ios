acd56344e95ca8b7030efc42ef0b9d4c0958ab61
TMDev24 <tm.dev12@outlook.com>
2024-08-14 19:48:13 +0100

    Add `ENDProductPresentationModelling`

---
+++ Feature Business Logic Layer/ENDListingsFeature/Sources/ENDListingsFeature/Presentation/Presentation Models/ENDProductPresentationModelling.swift
@@ -0,0 +1,15 @@
+//
+//  ENDProductPresentationModelling.swift
+//
+//
+//  Created by Tak Mazarura on 14/08/2024.
+//
+
+import Foundation
+
+public protocol ENDProductPresentationModelling {
+    var id: String { get }
+    var name: String { get }
+    var price: String { get }
+    var imageURL: URL { get }
+}
