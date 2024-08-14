436343fdb9a46b290308572e27cf385ca95b3b7b
TMDev24 <tm.dev12@outlook.com>
2024-08-14 20:50:22 +0100

    Add implementation for `ListingsCoordinator`

--- Presentation Layer/iOS/END-iOS/END-iOS/Feature UI/Listings Feed/View/Coordinator/ListingsCoordinator.swift
+++ Presentation Layer/iOS/END-iOS/END-iOS/Feature UI/Listings Feed/View/Coordinator/ListingsCoordinator.swift
@@ -12,3 +12,46 @@ import CoreNetworking
 import CorePresentation
 import ENDListingsFeature
 
+final class ListingsCoordinator: ChildCoordinatorable, ParentCoordinatorable {
+    var childCoordinators: [ChildCoordinatorable] = []
+    let client: HTTPClient
+    var router: Router
+    var parentCoordinator: ParentCoordinatorable?
+    
+    init(
+        router: Router,
+        client: HTTPClient,
+        parentCoordinator: ParentCoordinatorable?
+    ) {
+        self.router = router
+        self.client = client
+        self.parentCoordinator = parentCoordinator
+    }
+
+    func start() {
+        navigateToListingsFeed()
+    }
+    
+    func didEnd(childCoordinator: CorePresentation.ChildCoordinatorable) {
+        efficientPrint("Did close Child Coordinator")
+    }
+}
+ 
+// MARK: - ListingsFeedCoordinating
+extension ListingsCoordinator: ListingsFeedCoordinating {
+    func navigateToListingsFeed() {
+        let listingsFeedService = RemoteListingsFeedService(client: client)
+        let accountsFeedViewController = ListingsFeedComposer.compose(
+            with: listingsFeedService,
+            title: "Listings",
+            coordinator: self
+        )
+        router.navigateToViewController(accountsFeedViewController, withMethod: .push)
+    }
+    
+    func navigateToListingDetail(
+        forListing listing: ENDProductModel
+    ) {
+        
+    }
+}
