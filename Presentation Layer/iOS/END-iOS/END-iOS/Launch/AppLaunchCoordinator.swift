0a8919914d242a0e700c44f4142e243324096c00
TMDev24 <tm.dev12@outlook.com>
2024-08-14 20:40:51 +0100

    Add `AppLaunchCoordinator`

---
+++ Presentation Layer/iOS/END-iOS/END-iOS/Launch/AppLaunchCoordinator.swift
@@ -0,0 +1,38 @@
+//
+//  AppLaunchCoordinator.swift
+//  END-iOS
+//
+//  Created by Tak Mazarura on 14/08/2024.
+//
+
+import CoreFoundational
+import CoreNetworking
+import CorePresentation
+import ENDListingsFeature
+import UIKit
+
+final class AppLaunchCoordinator {
+    var router: Router
+    var childCoordinators = [ChildCoordinatorable]()
+    let client: HTTPClient
+    
+    init(router: Router, client: HTTPClient) {
+        self.router = router
+        self.client = client
+    }
+    
+    func start() {
+        navigateToAccountsFeed()
+    }
+    
+    func didEnd(childCoordinator: ChildCoordinatorable) {
+        efficientPrint("Did close parent coordinator")
+    }
+}
+
+// MARK: - ParentCoordinatorable
+extension AppLaunchCoordinator: ParentCoordinatorable {
+    func navigateToAccountsFeed() {
+      
+    }
+}
