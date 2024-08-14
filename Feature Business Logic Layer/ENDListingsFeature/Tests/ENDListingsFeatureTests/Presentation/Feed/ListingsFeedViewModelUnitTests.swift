b6dab981484adafd06dbbd72e655b0d42f734ad9
TMDev24 <tm.dev12@outlook.com>
2024-08-14 19:41:03 +0100

    Add empty implementations of `ListingsFeedViewModelUnitTests`

---
+++ Feature Business Logic Layer/ENDListingsFeature/Tests/ENDListingsFeatureTests/Presentation/Feed/ListingsFeedViewModelUnitTests.swift
@@ -0,0 +1,30 @@
+//
+//  ListingsFeedViewModelUnitTests.swift
+//
+//
+//  Created by Tak Mazarura on 14/08/2024.
+//
+
+import XCTest
+import CoreFoundational
+import CoreNetworking
+import MockNetworking
+@testable import ENDListingsFeature
+
+final class ListingsFeedViewModelUnitTests: ListingsFeedViewModelUnitTest {
+    func test_loadFeed_setsLoadStateToTrue() {
+        
+    }
+    
+    func test_loadFeed_triggersAPICall_whichSuccessOrFailure_setsLoadStateToFalse() {
+        
+    }
+    
+    func test_loadFeed_triggersAPICall_whichOnError_returnsError() {
+        
+    }
+    
+    func test_loadFeed_triggersAPICall_whichOnSuccess_returnsListings() {
+        
+    }
+}
