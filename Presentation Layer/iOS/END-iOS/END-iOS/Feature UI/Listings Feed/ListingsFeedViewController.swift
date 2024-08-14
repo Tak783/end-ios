237b68aa79f6ca8640fa59e703e2104255d31568
TMDev24 <tm.dev12@outlook.com>
2024-08-14 20:38:23 +0100

    Add `ListingsFeedViewController`

---
+++ Presentation Layer/iOS/END-iOS/END-iOS/Feature UI/Listings Feed/ListingsFeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListingsFeedViewController.swift
+//  END-iOS
+//
+//  Created by Tak Mazarura on 14/08/2024.
+//
+
+import UIKit
+import ENDListingsFeature
+
+final class ListingsFeedViewController: UIViewController {
+    var feedViewModel: ListingsFeedViewModelling?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
