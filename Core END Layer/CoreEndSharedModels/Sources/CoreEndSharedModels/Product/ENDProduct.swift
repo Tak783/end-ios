dc128dab6b4fe85aec1583aaaade426058c8f505
TMDev24 <tm.dev12@outlook.com>
2024-08-14 13:49:40 +0100

    Add `CoreEndSharedModels` package with new `ENDProduct` model and protocol

---
+++ Core END Layer/CoreEndSharedModels/Sources/CoreEndSharedModels/Product/ENDProduct.swift
@@ -0,0 +1,24 @@
+//
+//  ENDProduct.swift
+//
+//
+//  Created by Tak Mazarura on 14/08/2024.
+//
+
+public struct ENDProduct: Codable {
+    public let id: String
+    public let name: String
+    public let price: String
+    public let image: String
+    
+    public init(
+        id: String,
+        name: String,
+        price: String,
+        image: String) {
+        self.id = id
+        self.name = name
+        self.price = price
+        self.image = image
+    }
+}
---
+++ Core END Layer/CoreEndSharedModels/Sources/CoreEndSharedModels/Product/ENDProductModelling.swift
@@ -0,0 +1,13 @@
+//
+//  ENDProductModelling.swift
+//
+//
+//  Created by Tak Mazarura on 14/08/2024.
+//
+
+public protocol ENDProductModelling {
+    var id: String { get }
+    var name: String { get }
+    var price: String { get }
+    var image: String { get }
+}
